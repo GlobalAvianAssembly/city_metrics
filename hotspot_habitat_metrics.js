9bc6af6b111328806b115572081e6cdfaa401d72
jameselliot <jameselliot@gmail.com>
2021-01-28 15:00:03 +0000

    Updated hotspot_habitat_metrics.js (added 2 lines).

--- hotspot_habitat_metrics.js
+++ hotspot_habitat_metrics.js
@@ -29,6 +29,8 @@ var updated = hotspots.limit(200).map(function(feature) {
     new ee.Dictionary()
       .combine(metricsForBuffer(point, 1000, 'b1km'))
       .combine(metricsForBuffer(point, 2000, 'b2km'))
+      .combine(metricsForBuffer(point, 3000, 'b3km'))
+      .combine(metricsForBuffer(point, 4000, 'b4km'))
       .combine(metricsForBuffer(point, 5000, 'b5km'))
       .set('city_name', feature.get('NAME_MAIN'))
       .set('hotspot_id', feature.get('hotspot_id'))
