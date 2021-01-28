82a43b9284b926aacb6fe142030670f6995a0058
jameselliot <jameselliot@gmail.com>
2021-01-28 14:35:53 +0000

    Updated hotspot_habitat_metrics.js (added 1 line).

--- hotspot_habitat_metrics.js
+++ hotspot_habitat_metrics.js
@@ -6,6 +6,7 @@ var LandCoverage = require('users/jamesr/city_metrics:modules/LandCoverage.js');
 
 var updated = hotspots.map(function(feature) {
   var buffer_1km = feature.geometry().buffer(1000);
+  Landscape.
 });
 
 Map.addLayer(updated);
