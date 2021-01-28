9cfb2b105c14165a8c9d335e9e10ac9b65b59c03
jameselliot <jameselliot@gmail.com>
2021-01-28 14:38:15 +0000

    Updated hotspot_habitat_metrics.js (added 23 lines, deleted 3 lines).

--- hotspot_habitat_metrics.js
+++ hotspot_habitat_metrics.js
@@ -4,9 +4,29 @@ var hotspots = ee.FeatureCollection("users/jamesr/UrbanHotspots");
 
 var LandCoverage = require('users/jamesr/city_metrics:modules/LandCoverage.js');
 
+/*
+var buffer = polygon.buffer(100000).difference(polygon);
+  var frequency_region = LandCoverage.coverage(buffer, 300000000);
+  
+  return ee.Feature(
+    polygon, 
+    LandCoverage.metrics('city', frequency_city, polygon.area())
+    .combine(LandCoverage.metrics('region', frequency_region, buffer.area()))
+  )
+  */
+  
 var updated = hotspots.map(function(feature) {
   var buffer_1km = feature.geometry().buffer(1000);
-  Landscape.
+  var frequency = LandCoverage.coverage(buffer_1km, 100000);
+  var metrics = LandCoverage.metrics('b1km', frequency, buffer_1km.area());
+  
+  return ee.Feature(
+    feature.geometry(),
+    metrics
+    .set('city_name', feature.get('NAME_MAIN'))
+    .set('hotspot_id', feature.get('hotspot_id'))
+    .set('elevation', feature.get('ELEVATION'))
+  );
 });
 
 Map.addLayer(updated);
