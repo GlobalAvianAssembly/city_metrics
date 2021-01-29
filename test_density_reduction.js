e5875a47e1754f86ad7f41f05bc98c0a4721d547
jameselliot <jameselliot@gmail.com>
2021-01-29 10:38:53 +0000

    Updated test_density_reduction.js (added 4 lines, deleted 1 line).

---
+++ test_density_reduction.js
@@ -0,0 +1,4 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var hotspots = ee.FeatureCollection("users/jamesr/UrbanHotspots"),
+    populationDensity = ee.Image("users/jamesr/GHS_POP_E2015_GLOBE_R2019A_54009_250_V1_0");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
