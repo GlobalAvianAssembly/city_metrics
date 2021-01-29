eff3cc522235d62226e14901d20864e251d8e1e5
jameselliot <jameselliot@gmail.com>
2021-01-29 12:00:30 +0000

    Updated modules/LandCoverage.js (added 65 lines, deleted 66 lines).

--- modules/LandCoverage.js
+++ modules/LandCoverage.js
@@ -25,84 +25,83 @@ exports.coverage = function(polygon, maxPixels) {
   });
 }
 
+function getLandcover(frequencyDictionary, copernicusValue) {
+  return ee.Number(frequencyDictionary.get(copernicusValue, 0));
+}
+
+function appendNumber(dictionary, prefix, key, value) {
+  dictionary.set(prefix + key, value.format('%.5f'))
+}
+
+function appendPercent(dictionary, prefix, key, value, area) {
+  dictionary.set(prefix + key, value.divide(area).format('%.5f'))
+}
+
+function append(dictionary, prefix, key, value, area) {
+  appendNumber(dictionary, prefix, '_lc_' + key, value);
+  appendPercent(dictionary, prefix, '_pc_' + key, value, area);
+}
+
 exports.metrics = function(prefix, frequency, area) {
+  var landcover = new LandCoverFrequency(frequence);
+  
   var discrete = new ee.Dictionary(frequency.get('discrete_classification'));
   var totalLandCoverArea = discrete.values().reduce(ee.Reducer.sum());
   
-  var closedForestEvergreenNeedle = ee.Number(discrete.get('111', 0));
-  var closedForestEvergreenBroadleaf = ee.Number(discrete.get('112', 0));
-  var closedForestDeciduousNeedle = ee.Number(discrete.get('113', 0));
-  var closedForestDeciduousBroadleaf = ee.Number(discrete.get('114', 0));
-  var closedForestMixed = ee.Number(discrete.get('115', 0));
-  var closedForestOther = ee.Number(discrete.get('116', 0));
+  var closedForestEvergreenNeedle = getLandcover(discrete, '111');
+  var closedForestEvergreenBroadleaf = getLandcover(discrete, '112');
+  var closedForestDeciduousNeedle = getLandcover(discrete, '113');
+  var closedForestDeciduousBroadleaf = getLandcover(discrete, '114');
+  var closedForestMixed = getLandcover(discrete, '115');
+  var closedForestOther = getLandcover(discrete, '116');
   
   var closedForestTotal = closedForestEvergreenNeedle.add(closedForestEvergreenBroadleaf)
     .add(closedForestDeciduousNeedle).add(closedForestDeciduousBroadleaf).add(closedForestMixed)
     .add(closedForestOther);
     
-  var openForestEvergreenNeedle = ee.Number(discrete.get('121', 0));
-  var openForestEvergreenBroadleaf = ee.Number(discrete.get('122', 0));
-  var openForestDeciduousNeedle = ee.Number(discrete.get('123', 0));
-  var openForestDeciduousBroadleaf = ee.Number(discrete.get('124', 0));
-  var openForestMixed = ee.Number(discrete.get('125', 0));
-  var openForestOther = ee.Number(discrete.get('126', 0));
+  var openForestEvergreenNeedle = getLandcover(discrete, '121');
+  var openForestEvergreenBroadleaf = getLandcover(discrete, '122');
+  var openForestDeciduousNeedle = getLandcover(discrete, '123');
+  var openForestDeciduousBroadleaf = getLandcover(discrete, '124');
+  var openForestMixed = getLandcover(discrete, '125');
+  var openForestOther = getLandcover(discrete, '126');
   
   var openForestTotal = openForestEvergreenNeedle.add(openForestEvergreenBroadleaf)
     .add(openForestDeciduousNeedle).add(openForestDeciduousBroadleaf).add(openForestMixed)
     .add(openForestOther);
     
-  return new ee.Dictionary()
-    .set(prefix + '_lc_unknown', discrete.get('0', 0))
-    .set(prefix + '_pc_unknown', ee.Number(discrete.get('0', 0)).divide(totalLandCoverArea))
-    .set(prefix + '_lc_moss_and_lichen', discrete.get('100', 0))
-    .set(prefix + '_pc_moss_and_lichen', ee.Number(discrete.get('100', 0)).divide(totalLandCoverArea))
-    .set(prefix + '_lc_closed_forest_evergreen_needle', closedForestEvergreenNeedle)
-    .set(prefix + '_pc_closed_forest_evergreen_needle', closedForestEvergreenNeedle.divide(totalLandCoverArea))
-    .set(prefix + '_lc_closed_forest_evergreen_broadleaf', closedForestEvergreenBroadleaf)
-    .set(prefix + '_pc_closed_forest_evergreen_broadleaf', closedForestEvergreenBroadleaf.divide(totalLandCoverArea))
-    .set(prefix + '_lc_closed_forest_deciduous_needle', closedForestDeciduousNeedle)
-    .set(prefix + '_pc_closed_forest_deciduous_needle', closedForestDeciduousNeedle.divide(totalLandCoverArea))
-    .set(prefix + '_lc_closed_forest_deciduous_broadleaf', closedForestDeciduousBroadleaf)
-    .set(prefix + '_pc_closed_forest_deciduous_broadleaf', closedForestDeciduousBroadleaf.divide(totalLandCoverArea))
-    .set(prefix + '_lc_closed_forest_mixed', closedForestMixed)
-    .set(prefix + '_pc_closed_forest_mixed', closedForestMixed.divide(totalLandCoverArea))
-    .set(prefix + '_lc_closed_forest_other', closedForestOther)
-    .set(prefix + '_pc_closed_forest_other', closedForestOther.divide(totalLandCoverArea))
-    .set(prefix + '_lc_closed_forest_total', closedForestTotal)
-    .set(prefix + '_pc_closed_forest_total', closedForestTotal.divide(totalLandCoverArea))
-    .set(prefix + '_lc_open_forest_evergreen_needle', openForestEvergreenNeedle)
-    .set(prefix + '_pc_open_forest_evergreen_needle', openForestEvergreenNeedle.divide(totalLandCoverArea))
-    .set(prefix + '_lc_open_forest_evergreen_broadleaf', openForestEvergreenBroadleaf)
-    .set(prefix + '_pc_open_forest_evergreen_broadleaf', openForestEvergreenBroadleaf.divide(totalLandCoverArea))
-    .set(prefix + '_lc_open_forest_deciduous_needle', openForestDeciduousNeedle)
-    .set(prefix + '_pc_open_forest_deciduous_needle', openForestDeciduousNeedle.divide(totalLandCoverArea))
-    .set(prefix + '_lc_open_forest_deciduous_broadleaf', openForestDeciduousBroadleaf)
-    .set(prefix + '_pc_open_forest_deciduous_broadleaf', openForestDeciduousBroadleaf.divide(totalLandCoverArea))
-    .set(prefix + '_lc_open_forest_mixed', openForestMixed)
-    .set(prefix + '_pc_open_forest_mixed', openForestMixed.divide(totalLandCoverArea))
-    .set(prefix + '_lc_open_forest_other', openForestOther)
-    .set(prefix + '_pc_open_forest_other', openForestOther.divide(totalLandCoverArea))
-    .set(prefix + '_lc_open_forest_total', openForestTotal)
-    .set(prefix + '_pc_open_forest_total', openForestTotal.divide(totalLandCoverArea))
-    .set(prefix + '_lc_shrubs', discrete.get('20', 0))
-    .set(prefix + '_pc_shrubs', ee.Number(discrete.get('20', 0)).divide(totalLandCoverArea))
-    .set(prefix + '_lc_ocean', discrete.get('200', 0))
-    .set(prefix + '_pc_ocean', ee.Number(discrete.get('200', 0)).divide(totalLandCoverArea))
-    .set(prefix + '_lc_herbaceous_vegetation', discrete.get('30', 0))
-    .set(prefix + '_pc_herbaceous_vegetation', ee.Number(discrete.get('30', 0)).divide(totalLandCoverArea))
-    .set(prefix + '_lc_cultivated', discrete.get('40', 0))
-    .set(prefix + '_pc_cultivated', ee.Number(discrete.get('40', 0)).divide(totalLandCoverArea))
-    .set(prefix + '_lc_urban', discrete.get('50', 0))
-    .set(prefix + '_pc_urban', ee.Number(discrete.get('50', 0)).divide(totalLandCoverArea))
-    .set(prefix + '_lc_bare', discrete.get('60', 0))
-    .set(prefix + '_pc_bare', ee.Number(discrete.get('60', 0)).divide(totalLandCoverArea))
-    .set(prefix + '_lc_snow', discrete.get('70', 0))
-    .set(prefix + '_pc_snow', ee.Number(discrete.get('70', 0)).divide(totalLandCoverArea))
-    .set(prefix + '_lc_permanent_water', discrete.get('80', 0))
-    .set(prefix + '_pc_permanent_water', ee.Number(discrete.get('80', 0)).divide(totalLandCoverArea))
-    .set(prefix + '_lc_herbaceous_wetland', discrete.get('90', 0))
-    .set(prefix + '_pc_herbaceous_wetland', ee.Number(discrete.get('90', 0)).divide(totalLandCoverArea))
-    .set(prefix + '_calcuated_area', (area.divide(10000)))
-    .set(prefix + '_lc_area', totalLandCoverArea)
-  ;
+  var result = new ee.Dictionary();
+  append(result, prefix, 'unknown', getLandcover(discrete, '0'), totalLandCoverArea);
+  append(result, prefix, 'moss_and_lichen', getLandcover(discrete, '100'), totalLandCoverArea);
+  
+  append(result, prefix, 'closed_forest_evergreen_needle', closedForestEvergreenNeedle, totalLandCoverArea);
+  append(result, prefix, 'closed_forest_evergreen_broadleaf', closedForestEvergreenBroadleaf, totalLandCoverArea);
+  append(result, prefix, 'closed_forest_deciduous_needle', closedForestDeciduousNeedle, totalLandCoverArea);
+  append(result, prefix, 'closed_forest_deciduous_broadleaf', closedForestDeciduousBroadleaf, totalLandCoverArea);
+  append(result, prefix, 'closed_forest_forest_mixed', closedForestMixed, totalLandCoverArea);
+  append(result, prefix, 'closed_forest_forest_other', closedForestOther, totalLandCoverArea);
+  append(result, prefix, 'closed_forest_forest_total', closedForestTotal, totalLandCoverArea);
+  
+  append(result, prefix, 'open_forest_evergreen_needle', openForestEvergreenNeedle, totalLandCoverArea);
+  append(result, prefix, 'open_forest_evergreen_broadleaf', openForestEvergreenBroadleaf, totalLandCoverArea);
+  append(result, prefix, 'open_forest_deciduous_needle', openForestDeciduousNeedle, totalLandCoverArea);
+  append(result, prefix, 'open_forest_deciduous_broadleaf', openForestDeciduousBroadleaf, totalLandCoverArea);
+  append(result, prefix, 'open_forest_forest_mixed', openForestMixed, totalLandCoverArea);
+  append(result, prefix, 'open_forest_forest_other', openForestOther, totalLandCoverArea);
+  append(result, prefix, 'open_forest_forest_total', openForestTotal, totalLandCoverArea);
+  
+  append(result, prefix, 'shrubs', getLandcover(discrete, '20'), totalLandCoverArea);
+  append(result, prefix, 'ocean', getLandcover(discrete, '200'), totalLandCoverArea);
+  append(result, prefix, 'herbaceous_vegetation', getLandcover(discrete, '30'), totalLandCoverArea);
+  append(result, prefix, 'cultivated', getLandcover(discrete, '40'), totalLandCoverArea);
+  append(result, prefix, 'urban', getLandcover(discrete, '50'), totalLandCoverArea);
+  append(result, prefix, 'bare', getLandcover(discrete, '60'), totalLandCoverArea);
+  append(result, prefix, 'snow', getLandcover(discrete, '70'), totalLandCoverArea);
+  append(result, prefix, 'permanent_water', getLandcover(discrete, '80'), totalLandCoverArea);
+  append(result, prefix, 'herbaceous_wetland', getLandcover(discrete, '90'), totalLandCoverArea);
+ 
+  appendNumber(result, prefix, '_calcuated_area', area.divide(10000));
+  appendNumber(result, prefix, '_lc_area', totalLandCoverArea);
+  
+  return result;
 }
