1cb2d67167eea4a8dac7e66e2780d36b0407e3df
jameselliot <jameselliot@gmail.com>
2021-01-29 10:40:00 +0000

    Updated test_density_reduction.js (added 24 lines).

--- test_density_reduction.js
+++ test_density_reduction.js
@@ -2,3 +2,27 @@
 var hotspots = ee.FeatureCollection("users/jamesr/UrbanHotspots"),
     populationDensity = ee.Image("users/jamesr/GHS_POP_E2015_GLOBE_R2019A_54009_250_V1_0");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+
+function averagePopulationDensity(polygon) {
+  
+  var result = populationDensity.reduceRegion({
+    reducer: ee.Reducer.mean(),
+    geometry: polygon,
+    scale: 100,
+    maxPixels: 1e9
+  }).get('b1');
+  
+  return ee.Algorithms.If({
+    condition: result,
+    trueCase: result,
+    falseCase: 0
+  });
+}
+
+hotspots.filter(function(feature) {
+  return ee.Algorithms.If({
+    condition: feature.get,
+    trueCase: result,
+    falseCase: 0
+  });
+}
