594fab50ce2a9a7ef3b18e44f1db5c00754f6f08
jameselliot <jameselliot@gmail.com>
2021-03-25 10:15:03 +0000

    Updated species_pools.js (added 19 lines, deleted 1 line).

--- species_pools.js
+++ species_pools.js
@@ -3,4 +3,22 @@ var all_species = ee.FeatureCollection("users/jamesr/AllSpeciesClippedToUrbanAre
     urban_areas = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 
-Map.addLayer(all_species);
+// Define a spatial filter, with distance 100 km.
+var distFilter = ee.Filter.withinDistance({
+  distance: 1000,
+  leftField: '.geo',
+  rightField: '.geo',
+  maxError: 10
+});
+
+// Define a saveAll join.
+var distSaveAll = ee.Join.saveAll({
+  matchesKey: 'points',
+  measureKey: 'distance'
+});
+
+// Apply the join.
+var spatialJoined = distSaveAll.apply(urban_areas, all_species, distFilter);
+
+// Print the result.
+print(spatialJoined);
