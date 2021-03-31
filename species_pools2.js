ee278a4ef7316cca865e220d2f8d211a0b06a958
jameselliot <jameselliot@gmail.com>
2021-03-31 07:52:07 +0000

    Updated species_pools2.js (added 3 lines, deleted 4 lines).

--- species_pools2.js
+++ species_pools2.js
@@ -32,10 +32,9 @@ intersectJoined = intersectJoined.map(function(urban_area) {
   // Get "power_plant" intersection list, count how many intersected this state.
   var species = ee.List(urban_area.get('species'))
     .map(function(s) {
-      return ee.feature(s.geometry(), ee.Dictionary({
-        'city_name', urban_area.get('NAME_MAIN'),
-        'species', s.get('binomial')
-        }))
+      return ee.feature(s.geometry(), new ee.Dictionary()
+        .set('city_name', urban_area.get('NAME_MAIN')),
+        .set('species', s.get('binomial'));
     });
   // Return the state feature with a new property: power plant count.
   return ee.FeatureCollection(species);
