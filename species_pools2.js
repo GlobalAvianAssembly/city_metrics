c4e56f01a3992fc6f628b07e4fc7becd29d8adc2
jameselliot <jameselliot@gmail.com>
2021-03-31 07:26:29 +0000

    Updated species_pools2.js (added 22 lines).

--- species_pools2.js
+++ species_pools2.js
@@ -2,3 +2,25 @@
 var all_species = ee.FeatureCollection("users/jamesr/AllSpeciesClippedToUrbanArea"),
     urban_areas = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+
+var spatialFilter = ee.Filter.intersects({
+  leftField: '.geo',
+  rightField: '.geo',
+  maxError: 10
+});
+
+// Define a save all join.
+var saveAllJoin = ee.Join.saveAll({
+  matchesKey: 'species',
+});
+
+// Apply the join.
+var intersectJoined = saveAllJoin.apply(states, all_species, urban_areas);
+
+// Return list of species per urban area
+intersectJoined = intersectJoined.map(function(urban_area) {
+  // Get "power_plant" intersection list, count how many intersected this state.
+  var allSpecies = ee.List(urban_area.get('species'));
+  // Return the state feature with a new property: power plant count.
+  return allSpecies
+});
