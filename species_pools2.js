ea678f1846235d57ddb1ae4193628c9fa34abeb2
jameselliot <jameselliot@gmail.com>
2021-03-31 09:38:50 +0000

    Updated species_pools2.js (added 11 lines, deleted 6 lines).

--- species_pools2.js
+++ species_pools2.js
@@ -20,17 +20,22 @@ var species_subset = all_species.filterMetadata("binomial", "starts_with", "S");
 
 var intersectJoined = saveAllJoin.apply(urban_areas_subset, species_subset, spatialFilter);
 
-print(intersectJoined)
-
 // Return list of species per urban area
 var species_in_cities = intersectJoined.map(function(urban_area) {
-  return ee.List(urban_area.get('species'))
+  var species_in_city = ee.List(urban_area.get('species'))
     .map(function(species_area) {
       var species_area_f = ee.Feature(species_area);
-      return new ee.Dictionary()
+      return ee.Feature(urban_area.geometry(), new ee.Dictionary()
         .set('city_name', urban_area.get('NAME_MAIN'))
-        .set('species', species_area_f.get('binomial'));
+        .set('species', species_area_f.get('binomial'))
+        .set('presence', species_area_f.get('presence'))
+        .set('origin', species_area_f.get('origin'))
+        .set('seasonal', species_area_f.get('seasonal'))
+        .set('source', species_area_f.get('source'))
+        .set('citation', species_area_f.get('citation'))
+        .set('yrcompiled', species_area_f.get('yrcompiled')));
     });
+  return ee.FeatureCollection(species_in_city);
 });
 
 print(species_in_cities.flatten())
