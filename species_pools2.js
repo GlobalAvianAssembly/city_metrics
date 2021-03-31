218cd1dcfebd41fc6ad748bb5b585d7f6e67dfa4
jameselliot <jameselliot@gmail.com>
2021-03-31 07:24:27 +0000

    Updated species_pools2.js (added 1 line, deleted 1 line).

--- species_pools2.js
+++ species_pools2.js
@@ -1,4 +1,4 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
 var all_species = ee.FeatureCollection("users/jamesr/AllSpeciesClippedToUrbanArea"),
-    cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million");
+    urban_areas = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
