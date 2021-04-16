68d72430490b11984bb5a575d6c6904fbfea6d9f
jameselliot <jameselliot@gmail.com>
2021-04-16 15:58:34 +0000

    Updated distance_from_coast.js (added 5 lines, deleted 1 line).

---
+++ distance_from_coast.js
@@ -0,0 +1,5 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var coastline = ee.FeatureCollection("users/jamesr/GSHHS_l_L1_Coastline");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+
+Map.add(coastline)
