0adffc41d256553c6a4de9fe6d7ec8d4c5bebcc4
jameselliot <jameselliot@gmail.com>
2021-04-16 16:09:19 +0000

    Updated distance_from_coast.js (added 35 lines, deleted 2 lines).

--- distance_from_coast.js
+++ distance_from_coast.js
@@ -3,5 +3,38 @@ var coastline = ee.FeatureCollection("users/jamesr/GSHHS_l_L1_Coastline"),
     cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 
-Map.addLayer(coastline)
-Map.addLayer(cities)
+var spatialFilter = ee.Filter.withinDistance({
+  distance: 500000,
+  leftField: '.geo',
+  rightField: '.geo',
+  maxError: 10
+})
+
+//join the cities to coastline
+var joined = ee.Join.saveAll({
+  matchesKey: 'neighbors', 
+  measureKey: 'distance',
+  ordering: 'distance'
+}).apply({
+  primary: cities, 
+  secondary: coastline, 
+  condition: spatialFilter
+});
+
+var hasNearest = joined.map(function(f) {
+  var neighsSize = ee.List(f.get('neighbors')).size();
+  return f.set('neighsSize', neighsSize);
+}).filter(ee.Filter.gt('neighsSize', 1));
+
+Map.addLayer(hasNearest, {color: 'red'}, 'hasNearest');
+
+// Get distance to nearest point.
+var withNearestDist = hasNearest.map(function(f) {
+  var nearestDist = ee.Feature(ee.List(f.get('neighbors')).get(1))
+    .get('distance');
+  return f.set('nearestDist', nearestDist);
+});
+
+print(withNearestDist)
+
+print(cities.distance(coastline))
