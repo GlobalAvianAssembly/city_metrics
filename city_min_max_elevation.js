969b57ca22da594deb6a7498afa790b16f71b93e
jameselliot <jameselliot@gmail.com>
2021-04-20 11:08:08 +0000

    Updated city_min_max_elevation.js (added 10 lines, deleted 18 lines).

--- city_min_max_elevation.js
+++ city_min_max_elevation.js
@@ -3,21 +3,13 @@ var cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million"),
     elevation = ee.Image("users/jamesr/world_digital_elevation_model");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 
-var elevations = cities.map(function(feature) {
-  var polygon = feature.geometry();
-  
-  var discrete = elevation.reduceRegion({
-    reducer: ee.Reducer.frequencyHistogram(),
-    geometry: polygon,
-    scale: 100,
-    maxPixels: 15000000
-  });
-  
-  var minMaxElevation = ee.Number(discrete.values().reduce(ee.Reducer.minMax()));
-  
-  return feature
-    .set('elevation', minMaxElevation)
+var elevationDelta = elevation.reduceRegions(cities, ee.Reducer.minMax());
+
+Map.addLayer(elevationDelta);
+
+Export.table.toCloudStorage({
+  collection: elevationDelta,
+  description: 'Export-city-elevation-delta',
+  fileNamePrefix: 'ee_city_elevation_delta',
+  bucket:'urban_ebird'
 });
-
-
-Map.addLayer(elevation.reduceRegions(cities, ee.Reducer.minMax()));
