11e0cd0c59a99f000a2fcafd3fc573bbe3aa3395
jameselliot <jameselliot@gmail.com>
2021-04-20 10:55:01 +0000

    Updated city_min_max_elevation.js (added 13 lines).

--- city_min_max_elevation.js
+++ city_min_max_elevation.js
@@ -2,3 +2,16 @@
 var cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million"),
     elevation = ee.Image("users/jamesr/world_digital_elevation_model");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+
+var elevations = cities.map(function(feature) {
+  var polygon = feature.geometry();
+  
+  var frequency_city = LandCoverage.coverage(polygon, 15000000);
+  
+  var buffer = polygon.buffer(100000).difference(polygon);
+  var frequency_region = LandCoverage.coverage(buffer, 300000000);
+  
+  return feature
+    .set('min_elevation', feature.get('NAME_MAIN'))
+    .set('max_elevation', feature.get('POP_2015'));
+});
