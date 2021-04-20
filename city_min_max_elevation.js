a89bca82a5af5ed6430df1f352002c9fac3cb503
jameselliot <jameselliot@gmail.com>
2021-04-20 10:53:12 +0000

    Updated city_min_max_elevation.js (added 3 lines, deleted 1 line).

---
+++ city_min_max_elevation.js
@@ -0,0 +1,3 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
