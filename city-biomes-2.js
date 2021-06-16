4bd393a54cb3a24c81416f511ecbd4a3cbb99f35
jameselliot <jameselliot@gmail.com>
2021-06-16 10:30:25 +0000

    Updated city-biomes-2.js (added 20 lines, deleted 22 lines).

--- city-biomes-2.js
+++ city-biomes-2.js
@@ -2,30 +2,28 @@
 var cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million"),
     biome = ee.Image("OpenLandMap/PNV/PNV_BIOME-TYPE_BIOME00K_C/v01");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+var LandCoverage = require('users/jamesr/city_metrics:modules/LandCoverage.js');
 
-// Define a spatial filter, with distance 0 km.
-var distFilter = ee.Filter.withinDistance({
-  distance: 0,
-  leftField: '.geo',
-  rightField: '.geo',
-  maxError: 10
+  
+var stats = cities.map(function(feature) {
+  var polygon = feature.geometry();
+  var frequency_city = LandCoverage.coverage(polygon, 15000000);
+  
+  return ee.Feature(
+    null, 
+    LandCoverage.metrics('city', frequency_city, ee.Number(polygon.area()))
+  )
+  .set('city_name', feature.get('NAME_MAIN'))
+  .set('pop_2015', feature.get('POP_2015'));
 });
 
-// Define a saveAll join.
-var distSaveAll = ee.Join.saveAll({
-  matchesKey: 'points',
-  measureKey: 'distance'
-});
-
-// Apply the join.
-var spatialJoined = distSaveAll.apply(cities, biomes, distFilter);
+print(stats);
 
 Export.table.toCloudStorage({
-  collection: spatialJoined,
-  description: 'Export-city-biome',
-  fileNamePrefix: 'city-biome',
+  collection: stats,
+  description: 'Export-city-land-coverage-to-gcs',
+  fileNamePrefix: 'city_copernicus_land_coverage',
   bucket:'urban_ebird'
 });
 
-
-Map.addLayer(spatialJoined)
+Map.addLayer(stats);
