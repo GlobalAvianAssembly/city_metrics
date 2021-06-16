771c89021f770bf3f0d7373d3c503831a4d9fb60
jameselliot <jameselliot@gmail.com>
2021-06-16 10:23:52 +0000

    Updated city_biomes.js (added 26 lines, deleted 1 line).

--- city_biomes.js
+++ city_biomes.js
@@ -3,4 +3,29 @@ var biomes = ee.FeatureCollection("RESOLVE/ECOREGIONS/2017"),
     cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 
-Map.addLayer(biomes)
+// Define a spatial filter, with distance 0 km.
+var distFilter = ee.Filter.withinDistance({
+  distance: 0,
+  leftField: '.geo',
+  rightField: '.geo',
+  maxError: 10
+});
+
+// Define a saveAll join.
+var distSaveAll = ee.Join.saveAll({
+  matchesKey: 'points',
+  measureKey: 'distance'
+});
+
+// Apply the join.
+var spatialJoined = distSaveAll.apply(cities, biomes, distFilter);
+
+Export.table.toCloudStorage({
+  collection: spatialJoined,
+  description: 'Export-city-biome',
+  fileNamePrefix: 'city-biome',
+  bucket:'urban_ebird'
+});
+
+
+Map.addLayer(spatialJoined)
