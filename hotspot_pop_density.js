87525ceff6a194ea204a8975343c6ac9feb59dd2
jameselliot <jameselliot@gmail.com>
2021-09-22 13:49:23 +0000

    Deleted hotspot_pop_density.js.

--- hotspot_pop_density.js
+++
@@ -1,4 +0,0 @@
-/**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var imageCollection = ee.ImageCollection("CIESIN/GPWv411/GPW_Population_Density"),
-    hotspots = ee.FeatureCollection("users/jamesr/UrbanHotspots");
-/***** End of imports. If edited, may not auto-convert in the playground. *****/
