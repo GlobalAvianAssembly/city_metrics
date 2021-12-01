c9311f05eac3e29afdf9a1279414aeda7ef2fe89
jameselliot <jameselliot@gmail.com>
2021-12-01 20:04:09 +0000

    Updated city_population_density.js (added 18 lines, deleted 22 lines).

--- city_population_density.js
+++ city_population_density.js
@@ -1,3 +1,7 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var populationDensity = ee.Image("users/jamesr/GHS_POP_E2015_GLOBE_R2019A_54009_250_V1_0"),
+    cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 
 
 function aboveZero(x) {
@@ -31,36 +35,28 @@ function minMaxPopulationDensity(polygon) {
 
 var stats = cities.map(function(feature) {
   var polygon = feature.geometry();
-  var frequency_city = LandCoverage.coverage(polygon, 15000000);
-  
   var buffer = polygon.buffer(100000).difference(polygon);
-  var frequency_region = LandCoverage.coverage(buffer, 300000000);
   
-  var elevationMinMax_city = minMaxElevation(polygon);
-  var elevationMinMax_region = minMaxElevation(buffer);
+  var popDensMinMax_city = minMaxPopulationDensity(polygon);
+  var popDensMinMax_region = minMaxPopulationDensity(buffer);
   
   return ee.Feature(
     null, 
-    LandCoverage.metrics('city', frequency_city, ee.Number(polygon.area()))
-    .combine(LandCoverage.metrics('region', frequency_region, ee.Number(buffer.area())))
   )
-  .set('city_average_elevation', averageElevation(polygon))
-  .set('city_min_elevation', aboveZero(elevationMinMax_city.get('b1_min')))
-  .set('city_max_elevation', aboveZero(elevationMinMax_city.get('b1_max')))
-  .set('city_estuary_area', areaCoveredbyEstuaries(polygon))
-  .set('region_average_elevation', averageElevation(buffer).min(0))
-  .set('region_min_elevation', aboveZero(elevationMinMax_region.get('b1_min')))
-  .set('region_max_elevation', aboveZero(elevationMinMax_region.get('b1_max')))
-  .set('region_estuary_area', areaCoveredbyEstuaries(buffer))
-  .set('city_name', feature.get('NAME_MAIN'))
-  .set('pop_2015', feature.get('POP_2015'));
+  .set('city_average_pop_dens', averagePopulationDensity(polygon))
+  .set('city_min_pop_dens', aboveZero(popDensMinMax_city.get('b1_min')))
+  .set('city_max_pop_dens', aboveZero(popDensMinMax_city.get('b1_max')))
+  .set('region_average_pop_dens', averagePopulationDensity(buffer).min(0))
+  .set('region_min_pop_dens', aboveZero(popDensMinMax_region.get('b1_min')))
+  .set('region_max_pop_dens', aboveZero(popDensMinMax_region.get('b1_max')))
+  .set('city_name', feature.get('NAME_MAIN'));
 });
 
 
 Export.table.toCloudStorage({
   collection: stats,
-  description: 'Export-city-land-coverage-to-gcs',
-  fileNamePrefix: 'city_copernicus_land_coverage',
+  description: 'Export-city-population-density-to-gcs',
+  fileNamePrefix: 'city_copernicus_population_density',
   bucket:'urban_ebird'
 });
 
