d4a614d19edb4fc7e594c0310019f92a2a8dba86
jameselliot <jameselliot@gmail.com>
2021-12-03 17:15:02 +0000

    Updated city_weather.js (added 4 lines, deleted 1 line).

---
+++ city_weather.js
@@ -0,0 +1,4 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million"),
+    climate = ee.Image("WORLDCLIM/V1/BIO");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
