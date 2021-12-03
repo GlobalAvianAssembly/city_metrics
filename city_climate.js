ee5e07e8a790e31c0c7342800ec40d846e1dcb63
jameselliot <jameselliot@gmail.com>
2021-12-03 17:15:53 +0000

    Updated city_climate.js (added 7 lines).

--- city_climate.js
+++ city_climate.js
@@ -2,3 +2,10 @@
 var cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million"),
     climate = ee.Image("WORLDCLIM/V1/BIO");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+
+
+
+var viz = {opacity: 0.5};
+
+Map.addLayer(climate, viz);
+Map.addLayer(cities, {opacity: 0.3})
