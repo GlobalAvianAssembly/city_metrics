c1d3641f0f1a7a0e4594ce918a2d7492a25e94bd
jameselliot <jameselliot@gmail.com>
2021-12-15 12:08:28 +0000

    Updated city_productivity.js (added 13 lines, deleted 42 lines).

--- city_productivity.js
+++ city_productivity.js
@@ -2,8 +2,8 @@
 var ndvi = ee.ImageCollection("MODIS/006/MOD13Q1");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 
-function averagePopulationDensity(polygon) {
-  return populationDensity.reduceRegion({
+function averageNdvi(polygon) {
+  return ndvi.reduceRegion({
     reducer: ee.Reducer.mean(),
     geometry: polygon,
     scale: 100,
@@ -11,57 +11,28 @@ function averagePopulationDensity(polygon) {
   }).get('b1');
 }
 
-function minMaxPopulationDensity(polygon) {
-  return populationDensity.reduceRegion({
-    reducer: ee.Reducer.minMax(),
-    geometry: polygon,
-    scale: 100,
-    maxPixels: 1e9
-  });
-}
-
 var stats = cities.map(function(feature) {
   var polygon = feature.geometry();
   var buffer_20k = polygon.buffer(20000).difference(polygon);
   var buffer_50k = polygon.buffer(50000).difference(polygon);
   var buffer_100k = polygon.buffer(100000).difference(polygon);
-  
-  var popDensMinMax_city = minMaxPopulationDensity(polygon);
-  var popDensMinMax_region20k = minMaxPopulationDensity(buffer_20k);
-  var popDensMinMax_region50k = minMaxPopulationDensity(buffer_50k);
-  var popDensMinMax_region100k = minMaxPopulationDensity(buffer_100k);
-  
   return ee.Feature(
     null, 
     new ee.Dictionary()
   )
-  .set('city_average_pop_dens', averagePopulationDensity(polygon))
-  .set('city_min_pop_dens', popDensMinMax_city.get('b1_min'))
-  .set('city_max_pop_dens', popDensMinMax_city.get('b1_max'))
-  .set('region_20_average_pop_dens', averagePopulationDensity(buffer_20k))
-  .set('region_20_min_pop_dens', popDensMinMax_region20k.get('b1_min'))
-  .set('region_20_max_pop_dens', popDensMinMax_region20k.get('b1_max'))
-  .set('region_50_average_pop_dens', averagePopulationDensity(buffer_50k))
-  .set('region_50_min_pop_dens', popDensMinMax_region50k.get('b1_min'))
-  .set('region_50_max_pop_dens', popDensMinMax_region50k.get('b1_max'))
-  .set('region_100_average_pop_dens', averagePopulationDensity(buffer_100k))
-  .set('region_100_min_pop_dens', popDensMinMax_region100k.get('b1_min'))
-  .set('region_100_max_pop_dens', popDensMinMax_region100k.get('b1_max'))
+  .set('city_ndvi', averageNdvi(polygon))
+  .set('region_20_ndvi', averageNdvi(buffer_20k))
+  .set('region_50_ndvi', averageNdvi(buffer_50k))
+  .set('region_100_ndvi', averageNdvi(buffer_100k))
   .set('city_name', feature.get('NAME_MAIN'));
 });
 
 
 Export.table.toCloudStorage({
   collection: stats,
-  description: 'Export-city-population-density-to-gcs',
-  fileNamePrefix: 'city_population_density',
+  description: 'Export-city-ndvi-to-gcs',
+  fileNamePrefix: 'city_ndvi',
   bucket:'urban_ebird'
 });
 
-var viz = {palette: ['00FFFF', '0000FF'], opacity: 0.8, min: 1000, bands: ['b1']};
-
-Map.addLayer(populationDensity, viz);
-Map.addLayer(cities, {opacity: 0.3})
-
-
-Map.addLayer(table)
+Map.addLayer(stats)
