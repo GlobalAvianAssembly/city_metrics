0c3e4f1909cc6f886fed8571a281f9c6b0f09a03
jameselliot <jameselliot@gmail.com>
2021-12-15 12:05:09 +0000

    Updated city_productivity.js (added 2 lines, deleted 2 lines).

--- city_productivity.js
+++ city_productivity.js
@@ -1,7 +1,7 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var table = ee.FeatureCollection("WCMC/WDPA/current/polygons"),
-    ndvi = ee.ImageCollection("MODIS/006/MOD13Q1");
+var ndvi = ee.ImageCollection("MODIS/006/MOD13Q1");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+
 
 
 Map.addLayer(table)
