86084d14f020ff4872735e0f142fc463dcc95637
jameselliot <jameselliot@gmail.com>
2021-12-17 16:10:57 +0000

    Updated city_protected_areas.js (added 5 lines, deleted 1 line).

--- city_protected_areas.js
+++ city_protected_areas.js
@@ -1,1 +1,5 @@
-var protected_areas = ee.FeatureCollection("WCMC/WDPA/current/polygons");
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million"),
+    protected_areas = ee.Image("users/jamesr/ProtectedAreas2");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+
