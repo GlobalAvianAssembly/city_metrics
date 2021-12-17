213d0924bd5518627bfa8f50e2fde8b5ef423235
jameselliot <jameselliot@gmail.com>
2021-12-17 16:12:54 +0000

    Updated city_protected_areas.js (added 2 lines, deleted 2 lines).

--- city_protected_areas.js
+++ city_protected_areas.js
@@ -3,11 +3,11 @@ var cities = ee.FeatureCollection("users/jamesr/UrbanAreasOver2Million"),
     protected_areas = ee.Image("users/jamesr/ProtectedAreas2");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 
-protected_area_coverage = function(polygon, maxPixels) {
+protected_area_coverage = function(polygon) {
   return protected_areas.reduceRegion({
     reducer: ee.Reducer.frequencyHistogram(),
     geometry: polygon,
     scale: 100,
-    maxPixels: maxPixels
+    maxPixels: 300000000
   });
 }
