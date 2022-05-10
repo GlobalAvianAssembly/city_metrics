5af6b78bf7ed8da1bb9e514f757bfebc099f78f7
jameselliot <jameselliot@gmail.com>
2022-05-10 16:37:30 +0000

    Updated modules/LandCoverageUK.js (added 1 line, deleted 1 line).

--- modules/LandCoverageUK.js
+++ modules/LandCoverageUK.js
@@ -2,6 +2,6 @@
 var landcover_1 = ee.ImageCollection("COPERNICUS/Landcover/100m/Proba-V-C3/Global");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 
-function create_buffer_around_point(point) {
+function create_buffer_around_point(point, buffer_metres) {
   
 }
