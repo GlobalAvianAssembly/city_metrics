9cd1795b056ae74964ba29dc2be5cb6b36f1fd0e
jameselliot <jameselliot@gmail.com>
2022-10-01 19:10:55 +0000

    Updated ssm_image_download.js (added 8 lines).

--- ssm_image_download.js
+++ ssm_image_download.js
@@ -2,4 +2,12 @@
 var ssm = ee.ImageCollection("NASA_USDA/HSL/SMAP10KM_soil_moisture")
   .select("ssm");
 
+print(ssm);
+
 Map.addLayer(ssm);
+
+
+Export.image.toDrive({
+  image: ssm,
+  description: 'World SSM'
+});
