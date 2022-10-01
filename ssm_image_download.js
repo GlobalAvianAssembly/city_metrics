921fb4753ad870249db8507aca9570c238562c87
jameselliot <jameselliot@gmail.com>
2022-10-01 19:04:36 +0000

    Updated ssm_image_download.js (added 5 lines, deleted 1 line).

---
+++ ssm_image_download.js
@@ -0,0 +1,5 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var ssm = ee.ImageCollection("NASA_USDA/HSL/SMAP10KM_soil_moisture");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+
+Map.addLayer(ssm);
